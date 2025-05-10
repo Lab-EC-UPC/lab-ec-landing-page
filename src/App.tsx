eb4a041cee7bb1c68a1215863632e283142ecff7
sayufu <lilianafuye@gmail.com>
2025-05-10 10:50:53 -0500

    fixed responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -22,14 +22,14 @@ function App() {
         <div className="mx-auto container px-4 md:px-0 py-12">
           <div className="flex w-full justify-center">
             <div className="flex flex-col items-center w-full md:w-[80%] gap-[20px] text-center">
-              <h1 className="text-5xl md:text-[64px] font-bold md:leading-[64px]">
+              <h1 className="text-3xl md:text-[64px] font-bold md:leading-[64px]">
                 Laboratorio de{" "}
                 <span className="font-bold bg-black from-primary to-secondary bg-clip-text text-transparent">
                   Periodismo Innovador
                 </span>
                 {" "}de ECData y la Universidad Peruana de Ciencias Aplicadas
               </h1>
-              <p className="text-placeholder text-xl md:text-2xl">
+              <p className="text-placeholder text-lg md:text-2xl">
                 Somos un espacio dedicado a potenciar la creatividad y la innovación en el periodismo, explorando el poder de los datos, las nuevas tecnologías y las historias digitales.
               </p>
             </div>
@@ -37,7 +37,7 @@ function App() {
         </div>
 
         <div className="mx-auto container px-4 md:px-0 mb-8 md:mb-12 w-full flex flex-col items-center py-8">
-          <div className="flex gap-4 w-fit px-48 py-[16px] text-black border-t-2 border-b-2 border-primary items-center">
+          <div className="flex gap-4 w-fit px-12 md:px-48 py-[16px] text-black border-t-2 border-b-2 border-primary items-center">
             <h1 className="text-lg font-bold">Nuestros proyectos</h1>
             <FaAngleDoubleDown size={16} className="text-primary animate-bounce" />
           </div>
@@ -98,7 +98,7 @@ function App() {
                 Especial Ciberseguridad
               </a>
             </div>
-            <div className="space-y-2 text-end">
+            <div className="space-y-2 text-center md:text-end">
               <h1 className="font-bold text-lg">Lab ECData - UPC © 2025</h1>
               <p>
                 Developed by{" "}
